b7f6c00983e95090a0c64256a76ec949a7a262d2
Danot3 <34762548+Danot3@users.noreply.github.com>
2021-09-21 15:00:42 -0400

    completed rough draft of challenge
    rough draft of challenge, require more polishing if time permitted

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # shopify_infastructure_challenge
- 
+
+
+usage:
+    initialize by running server.py
+
+    open a web browser at the address : http://127.0.0.1:5000
+
+    this launches the webpage on which the image repo is found
+
+
+Description:
+    Using Python Flask to host a web service and sqlite3 as the database
+    image repository for any images
+
+
+
---
+++ server.py
@@ -0,0 +1,50 @@
+from flask import Flask, render_template
+import sqlite3 as sql
+
+app = Flask(__name__)
+
+def get_cursor():
+    conn = sql.connect("database.db")
+    cur = conn.cursor()
+    return (cur, conn)
+
+
+
+def initialize_db():
+    (cur, conn) = get_cursor()
+
+    # Create products table with sample data
+    cur.execute("DROP TABLE IF EXISTS products")
+    cur.execute("CREATE TABLE products (name TEXT, imgpath TEXT)")
+    cur.execute("""INSERT INTO products (name, imgpath) VALUES \
+        ('Black Hoodie', 'images/black_hoodie.jpg'), \
+        ('Black Tee Shirt', 'images/black_tee_shirt.jpg'), \
+        ('Black Jeans', 'images/black_jeans.jpg'), \
+        ('White Sneakers', 'images/white_sneakers.jpg')""")
+
+    
+    conn.commit()
+    
+
+@app.route("/")
+def home_page():
+    (cur, _) = get_cursor()
+    cur.execute("SELECT rowid, * FROM products")
+    
+    rows = cur.fetchall()
+    print("Retrieved %d database entries" % len(rows))
+    
+    # Pre-process product info for HTML templates
+    products = []
+    for row in rows:
+        products.append({
+            "id":    row[0],
+            "name":  row[1],
+            "src":   "/static/%s" % (row[2]),
+        })
+
+    return render_template("index.html", products=products)
+ 
+if __name__ == '__main__':
+    initialize_db()
+    app.run(debug = True)
